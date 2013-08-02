37060b5e68d374366b5e2aa4b27a38adf306da15
ahsan-ali <nandoliyaahsan@gmail.com>
2013-08-02 10:10:45 +0530

    log4j example

---
+++ nandoliya/src/main/java/com/nandoliya/App.java
@@ -0,0 +1,18 @@
+package com.nandoliya;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+
+	private static final Logger logger=Logger.getLogger(App.class);
+	public static void main(String[] args) {
+		logger.info("main method");
+		logger.debug("debuging..");
+		System.out.println("Hello World!");
+	}
+}
